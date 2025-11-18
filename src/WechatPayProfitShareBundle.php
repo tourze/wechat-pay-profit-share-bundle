e9612d152f7203739d74019ba4f7250e3d2c7a38
github-ci <you@example.com>
2025-11-18 22:54:34 +0800

    fix test

--- src/WechatPayProfitShareBundle.php
+++ src/WechatPayProfitShareBundle.php
@@ -7,6 +7,9 @@ namespace Tourze\WechatPayProfitShareBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineSnowflakeBundle\DoctrineSnowflakeBundle;
+use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\DoctrineUserBundle\DoctrineUserBundle;
 use WechatPayBundle\WechatPayBundle;
 
 class WechatPayProfitShareBundle extends Bundle implements BundleDependencyInterface
@@ -16,6 +19,9 @@ class WechatPayProfitShareBundle extends Bundle implements BundleDependencyInter
         return [
             WechatPayBundle::class => ['all' => true],
             DoctrineBundle::class => ['all' => true],
+            DoctrineSnowflakeBundle::class => ['all' => true],
+            DoctrineTimestampBundle::class => ['all' => true],
+            DoctrineUserBundle::class => ['all' => true],
         ];
     }
 }
